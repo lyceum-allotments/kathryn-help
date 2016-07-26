250702fa59a0092c67b18dd98002ee2c99c3f6e3
John Sharp <john.sharp@acano.com>
2016-07-26 14:05:47 +0100

    changed plot_E_initial_vs_E_final graph to have different colours for different max steps

--- plot_E_initial_vs_E_final.py
+++ plot_E_initial_vs_E_final.py
@@ -3,15 +3,23 @@ import pickle
 
 fp = open("results.pckl")
 results = pickle.load(fp)
+graph_formats = {100: 'rv', 500: 'b+', 1000: 'gD', 2000: 'kx'}
 
-E_initial_series = []
-E_final_series = []
+def plot_E_initial_vs_E_final(max_steps):
+    E_initial_series = []
+    E_final_series = []
+    plot_results = filter(lambda x : (int(x["max_steps"]) == max_steps), results)
 
-for r in results:
-    E_initial_series.append(r["E_min_step"][0])
-    E_final_series.append(r["E_min"])
+    for p in plot_results:
+        E_initial_series.append(p["E_min_step"][0])
+        E_final_series.append(p["E_min"])
 
-plt.plot(E_initial_series, E_final_series, 'bo')
+    plt.plot(E_initial_series, E_final_series, graph_formats[max_steps], fillstyle='none', ms=8, markeredgewidth=2, label = "%d steps" % max_steps)
+
+for total_steps in [100, 500, 1000, 2000]:
+    plot_E_initial_vs_E_final(total_steps)
+
 plt.xlabel('E initial (eV)')
 plt.ylabel('E final (eV)')
+plt.legend()
 plt.show()
