ef1d0155a64ac34e598dfd81ac828a83e1f516f8
John Sharp <john.sharp@acano.com>
2016-07-28 13:39:26 +0100

    e intial vs e final uses html colour codes

--- plot_E_initial_vs_E_final.py
+++ plot_E_initial_vs_E_final.py
@@ -3,7 +3,10 @@ import pickle
 
 fp = open("results.pckl")
 results = pickle.load(fp)
-graph_formats = {100: 'rv', 500: 'b+', 1000: 'gD', 2000: 'kx'}
+graph_formats = {100: {'fmt' : 'v', 'color' : '#ff0000'},
+        500: {'fmt' : '+', 'color' : '#00ff00'},
+        1000: {'fmt' : 'D', 'color' : '#0000ff'},
+        2000: {'fmt' : 'x', 'color' : '#000000'}}
 
 def plot_E_initial_vs_E_final(max_steps):
     E_initial_series = []
@@ -14,7 +17,7 @@ def plot_E_initial_vs_E_final(max_steps):
         E_initial_series.append(p["E_min_step"][0])
         E_final_series.append(p["E_min"])
 
-    plt.plot(E_initial_series, E_final_series, graph_formats[max_steps], fillstyle='none', ms=8, markeredgewidth=2, label = "%d steps" % max_steps)
+    plt.plot(E_initial_series, E_final_series, graph_formats[max_steps]['fmt'], fillstyle='none', ms=8, markeredgewidth=2, label = "%d steps" % max_steps, color = graph_formats[max_steps]['color'])
 
 for total_steps in [100, 500, 1000, 2000]:
     plot_E_initial_vs_E_final(total_steps)
