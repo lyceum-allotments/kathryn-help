3624bb01472ded913647c6bb6312eef74db48495
John Sharp <john.sharp@acano.com>
2016-09-01 09:50:17 +0100

    changed plot_E_final_vs_num_structures.py to group on std_order number

--- plot_E_final_vs_num_structures.py
+++ plot_E_final_vs_num_structures.py
@@ -4,6 +4,7 @@ import numpy as np
 import use_tex_font as utf
 import matplotlib
 import pickle
+import itertools
 
 utf.use_tex_font()
 matplotlib.rcParams.update({'font.size':9})
@@ -13,11 +14,17 @@ fig, ax=plt.subplots(figsize = (2.74, 6.05))
 fp = open("results.pckl")
 results = pickle.load(fp)
 
+results.sort(lambda a, b : 1 if a["std_order"] > b["std_order"] else -1)
+std_order_grouped_results = []
+for k, g in itertools.groupby(results, lambda a : a["std_order"]):
+    gl = list(g)
+    std_order_grouped_results.append({"number_of_structs" : gl[0]["number_of_structs"], "E_min" : min([r["E_min"] for r in gl])})
+
 data = [[],[],[],[]] # data[0] will contain all energies of runs with T_reduced = 0.001,
                   # data[1] with T_reduced = 0.01, data[2] with T_reduced = 0.1, data[3] with T_reduced = 1.0 
 num_structures_lookup = {1 : 0, 2 : 1, 4 : 2, 8 : 3}
 
-for r in results:
+for r in std_order_grouped_results:
     index = num_structures_lookup[r["number_of_structs"]]
     data[index].append(r["E_min"])
 
